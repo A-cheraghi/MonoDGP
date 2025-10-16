427e0baa0b9e720b5397dee217a553a43f3d4e8a
Ali Cheraghi <Cheraghiali4@gmail.com>
2025-10-16 19:06:38 +0330

    std7

--- lib/helpers/decode_helper.py
+++ lib/helpers/decode_helper.py
@@ -28,7 +28,7 @@ def decode_detections(dets, info, calibs, cls_mean_size, threshold):
         #         threshold = mean_score
 
         if std > 0.1:
-            new_threshold = mean_score + (std / 9)
+            new_threshold = mean_score + (std / 7)
         else:
             new_threshold = threshold
     
