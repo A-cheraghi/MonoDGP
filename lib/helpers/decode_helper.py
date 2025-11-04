57f63e34ab3d8b5425345253b0027cdf4d17e279
Ali Cheraghi <Cheraghiali4@gmail.com>
2025-11-04 22:55:18 +0330

    show sorted score

--- lib/helpers/decode_helper.py
+++ lib/helpers/decode_helper.py
@@ -154,7 +154,7 @@ def decode_detections(dets, info, calibs, cls_mean_size, threshold):
         # std = np.std(score_all_np)
         # print(f"image  {info['img_id'][i]} _ mean => {np.mean(score_all):0.3f} _ mad => {robust_value:0.3f} _ STD => {std:0.3f}\n")          
 #####################################################
-        # print(f"image  {info['img_id'][i]}\n" , sorted(score_all, reverse=True))          
+        print(f"image  {info['img_id'][i]}\n" , sorted(score_all, reverse=True))          
         # print(f"image  {info['img_id'][i]}\n" , np.mean(score_all))     
         
 
